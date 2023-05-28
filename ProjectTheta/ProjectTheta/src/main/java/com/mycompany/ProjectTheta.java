f78dc4ef95b917392c008bf2800f24bcb0c46308
QuantumRevenant <sebaperales14@gmail.com>
2023-05-28 11:39:06 -0500

    feat(MODEL): :sparkles: Añadimos Sha256 en Models, con un metodo estatico rapido de usar

--- ProjectTheta/ProjectTheta/src/main/java/com/mycompany/ProjectTheta.java
+++ ProjectTheta/ProjectTheta/src/main/java/com/mycompany/ProjectTheta.java
@@ -3,6 +3,7 @@
  */
 package com.mycompany;
 
+import com.mycompany.model.Sha256;
 import com.mycompany.view.StartMenu;
 
 /**
@@ -12,6 +13,7 @@ import com.mycompany.view.StartMenu;
 public class ProjectTheta {
 
     public static void main(String[] args) {
+        System.out.println(Sha256.sha256("Hello World!"));
         StartMenu.main(args);
     }
 }
