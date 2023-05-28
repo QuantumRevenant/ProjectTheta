ac11b414799d15aaebd969aa443ccda6f036f316
QuantumRevenant <sebaperales14@gmail.com>
2023-05-27 22:10:11 -0500

    feat(MODEL): :sparkles: Añadido el Model de Mesa (WIP)

---
+++ ProjectTheta/src/main/java/model/Mesa.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author sebap
+ */
+public class Mesa {
+     private int codigo ;     
+     private MESA_STATUS mesa_status;
+     private Long codigoPedido;
+     
+     private static enum MESA_STATUS{
+         LIBRE,OCUPADA
+     }
+     
+}
